a63be531b3da601d75acf96d5580028d07a4115e
Vennela Chava <chava.vinni@gmail.com>
2023-03-02 03:54:00 -0500

    Time: 0 ms (100.00%), Space: 6 MB (52.06%) - LeetHub

---
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string categorizeBox(int length, int width, int height, int mass) {
+        long long int vol;
+        bool bulky=false, heavy=false;
+        if(mass>=100){
+            heavy=true;
+        }
+        if(max(length, max(width, height))>=10000){
+            bulky=true;
+        }
+        else{
+            vol=(long long int)length*(long long int)width*(long long int)height;
+            if(vol>=1000000000){
+                bulky=true;
+            }
+        }
+        if(heavy && bulky){
+            return "Both";
+        }
+        if(!(heavy || bulky)){
+            return "Neither";
+        }
+        if(heavy){
+            return "Heavy";
+        }
+        return "Bulky";
+    }
+};
