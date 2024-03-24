61938fd6fe2106aef8deb6308e67ddb374f3900f
Vennela Chava <chava.vinni@gmail.com>
2024-03-23 20:54:15 -0400

    Time: 70 ms (87.03%), Space: 63.7 MB (49.39%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int hare=nums[0], tort=nums[0];
+        do{
+            hare=nums[nums[hare]];
+            tort=nums[tort];
+        }while(hare!=tort);
+        tort=nums[0];
+        while(hare!=tort){
+            hare=nums[hare];
+            tort=nums[tort];
+        }
+        return hare;
+    }
+};
