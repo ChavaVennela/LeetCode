3dd32f7b364c5f77098c4c76a95b3e038c93c19c
Vennela Chava <chava.vinni@gmail.com>
2024-04-01 21:57:46 -0400

    Time: 39 ms (98.26%), Space: 48.2 MB (92.41%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -10,8 +10,8 @@ var expect = function(val) {
             }
             return true;
         },
-        notToBe:(val2)=>{
-            if(val===val2){
+        notToBe:(val1)=>{
+            if(val===val1){
                 throw new Error("Equal");
             }
             return true;
