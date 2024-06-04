db860bc7d523b3680c544da3552acc8ff269b91c
Vennela Chava <chava.vinni@gmail.com>
2024-06-04 06:32:05 -0700

    Time: 3 ms (55.35%), Space: 9 MB (26.04%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map<char, int>a;
+        for(int i=0; i<s.size(); i++){
+            a[s[i]]++;
+        }
+        int odd=0, count=0;
+        for(auto it=a.begin(); it!=a.end(); it++){
+            if(it->second%2==0){
+                count+=it->second;
+            }
+            else{
+                count+=it->second-1;
+                odd=1;
+            }
+        }
+        return odd+count;
+    }
+};
