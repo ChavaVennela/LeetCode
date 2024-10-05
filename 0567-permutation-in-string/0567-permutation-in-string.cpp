b3d328b693a26906910a00fbfe67760c5b5004ef
Vennela Chava <chava.vinni@gmail.com>
2024-10-05 17:58:30 -0500

    Time: 1083 ms (5.09%), Space: 60.7 MB (11.04%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int i, j;
+        bool ans;
+        if(s2.size() <s1.size()){
+            return false;
+        }
+        sort(s1.begin(), s1.end());
+        for(i=0; i<=s2.size()-s1.size(); i++){
+            string s = s2.substr(i,s1.size());
+            sort(s.begin(), s.end());
+            if(s==s1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
