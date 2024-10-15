81db8a0b5c0156183f3a92dafbbbddec6845c49b
Vennela Chava <chava.vinni@gmail.com>
2024-10-15 09:35:41 -0500

    Time: 35 ms (43.16%), Space: 15.7 MB (45.68%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        stack<int>a;
-        long long ans=0;
+        long long ans=0, count=0;
         for(int i=0; i<s.size(); i++){
-            if(s[i]=='1'){
-                a.push(1);
+            if(s[i]=='0'){
+                ans+=count;
             }
             else{
-                if(!a.empty()){
-                    a.push(0);
-                }
+                count++;
             }
-        }
-        while(!a.empty() && a.top()==1){
-            a.pop();
-        }
-        long long temp=0;
-        while(!a.empty()){
-            if(a.top()==0){
-                temp++;
-            }
-            else{
-                ans+=temp;
-            }
-            a.pop();
         }
         return ans;
     }
