efc16c6cc65f45674e3e875d1bff36d5ce0bd65b
Vennela Chava <chava.vinni@gmail.com>
2024-10-25 09:29:33 -0500

    Time: 47 ms (95.60%), Space: 47.8 MB (72.53%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -1,51 +1,15 @@
 class Solution {
 public:
-    static int count(string a){
-        int ct=0;
-        for(int i=0; i<a.size(); i++){
-            if(a[i]=='/'){
-                ct++;
-            }
-        }
-        return ct;
-    }
-    static bool cmp(string a, string b){
-        if(count(a) != count(b)){
-            return count(a)<count(b);
-        }
-        return a<b;
-    }
     vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin(), folder.end(), cmp);
-        unordered_map<string, int>a;
-        a[folder[0]]++;
-        for(int i=1; i<folder.size(); i++){
-            if(count(folder[i])>1){
-                string temp="/";
-                int flag=0;
-                for(int j=1; j<folder[i].size(); j++){
-                    if(folder[i][j]!='/'){
-                        temp+=folder[i][j];
-                    }
-                    else{
-                        if(a.find(temp) != a.end()){
-                            flag=1;
-                            break;
-                        }
-                        temp+=folder[i][j];
-                    }
-                }
-                if(flag==0){
-                    a[temp]++;
-                }
-            }
-            else{
-                a[folder[i]]++;
-            }
-        }
+        sort(folder.begin(), folder.end());
         vector<string>ans;
-        for(auto it=a.begin(); it!=a.end(); it++){
-            ans.push_back(it->first);
+        ans.push_back(folder[0]);
+        for(int i=1; i<folder.size(); i++){
+            string temp=ans.back();
+            temp+='/';
+            if(folder[i].compare(0, temp.size(), temp) !=0){
+                ans.push_back(folder[i]);
+            }
         }
         return ans;
     }
