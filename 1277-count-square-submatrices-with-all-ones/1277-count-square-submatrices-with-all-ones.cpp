ac2e7cb9b2922b425594bfd9cddb76b731e2d50b
Vennela Chava <chava.vinni@gmail.com>
2024-10-27 23:40:35 -0500

    Time: 16 ms (10.29%), Space: 28.9 MB (68.73%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& grid,
-              vector<vector<int>>& dp) {
-        // If the cell lies outside the grid, return 0.
-        if (i >= grid.size() || j >= grid[0].size()) return 0;
-        if (grid[i][j] == 0) return 0;
-
-        // If we have already visited this cell, return the memoized value.
-        if (dp[i][j] != -1) return dp[i][j];
-
-        // Find the answer for the cell to the right of the current cell.
-        int right = solve(i, j + 1, grid, dp);
-        // Find the answer for the cell to the diagonal of the current cell.
-        int diagonal = solve(i + 1, j + 1, grid, dp);
-        // Find the answer for the cell below the current cell.
-        int below = solve(i + 1, j, grid, dp);
-
-        return dp[i][j] = 1 + min(right, min(diagonal, below));
+    int check(int row, int col, vector<vector<int>>& matrix, vector<vector<int>>& dp){
+        if(row>=matrix.size()){
+            return 0;
+        }
+        if(col>=matrix[0].size()){
+            return 0;
+        }
+        if(dp[row][col]!=-1){
+            return dp[row][col];
+        }
+        if(matrix[row][col]==0){
+            return 0;
+        }
+        int down=check(row+1, col, matrix, dp);
+        int right=check(row, col+1, matrix, dp);
+        int diag=check(row+1, col+1, matrix, dp);
+        dp[row][col]=1+min(right, min(down, diag));
+        return dp[row][col];
     }
-
-    int countSquares(vector<vector<int>>& grid) {
-        int ans = 0;
-        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                ans += solve(i, j, grid, dp);
+    int countSquares(vector<vector<int>>& matrix) {
+        vector<vector<int>>dp(matrix.size(), vector<int>(matrix[0].size(), -1));
+        int ans=0;
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                ans+=check(i, j, matrix, dp);
             }
         }
         return ans;
